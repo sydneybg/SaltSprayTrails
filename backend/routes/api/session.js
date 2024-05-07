f695b00f8f2a7457f5725e9ddcd0a0d3a002f36c
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-05-07 19:56:51 -0400

    Updated routes to include name

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -47,6 +47,8 @@ router.post(
       id: user.id,
       email: user.email,
       username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
     };
 
     await setTokenCookie(res, safeUser);
@@ -77,6 +79,8 @@ router.get(
         id: user.id,
         email: user.email,
         username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName,
       };
       return res.json({
         user: safeUser
