f8753bf5caa8e717557f188851b6fce61b666f02
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-05-08 19:19:05 -0400

    Added Redux Store and Tested It

---
+++ frontend/src/store/store.js
@@ -0,0 +1,28 @@
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
+const rootReducer = combineReducers({
+  // ADD REDUCERS HERE
+});
+
+let enhancer;
+if (import.meta.env.MODE === "production") {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const logger = (await import("redux-logger")).default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+const configureStore = (preloadedState) => {
+  const store = createStore(rootReducer, preloadedState, enhancer);
+
+  if(process.env.NODE_ENV !== 'production') {
+    window.store = store;
+  }
+
+return store;
+};
+
+export default configureStore;
