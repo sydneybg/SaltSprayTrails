12649efa3d6776beb32b9dd5963ec2af5a40c589
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-05-09 19:17:50 -0400

    Added useref to hide the dropdown menu in the profile button

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { FaUserCircle } from 'react-icons/fa';
+import 'Navigation.css';
 import * as sessionActions from '../../store/session';
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
+  const ulRef = useRef(); //for hidding the dropdown
 
   const openMenu = () => {
     if (showMenu) return;
@@ -21,20 +23,29 @@ function ProfileButton({ user }) {
     dispatch(sessionActions.logout());
   };
 
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
+  const handleClick = (e) => {
+    if (!ulRef.current.contains(e.target)) {
+      closeMenu();
+    }
+  };
+
   return (
     <>
       <button onClick={openMenu}>
         <FaUserCircle />
       </button>
+            <ul className={ulClassName} ref={ulRef}>
+        <li>{user.username}</li>
+        <li>{user.firstName} {user.lastName}</li>
+        <li>{user.email}</li>
+        <li>
+          <button onClick={logout}>Log Out</button>
+        </li>
+      </ul>
       {showMenu && (
-        <ul className="profile-dropdown">
-          <li>{user.username}</li>
-          <li>{user.firstName} {user.lastName}</li>
-          <li>{user.email}</li>
-          <li>
-            <button onClick={logout}>Log Out</button>
-          </li>
-        </ul>
+        <div onClick={handleClick} className="menu-backdrop" />
       )}
     </>
   );
