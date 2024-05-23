f2ed0fe26f069cd865c52bc20a698984a59a8be9
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-05-23 19:24:19 -0400

    Commenting out all reviews

--- backend/db/migrations/20240513234230-create-review.js
+++ backend/db/migrations/20240513234230-create-review.js
@@ -1,62 +1,62 @@
-'use strict';
+// 'use strict';
 
-let options = {};
-if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;
-}
+// let options = {};
+// if (process.env.NODE_ENV === 'production') {
+//   options.schema = process.env.SCHEMA;
+// }
 
 
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Reviews', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      userId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'Users',
-          key: 'id'
-        },
-        onDelete: 'CASCADE'
-      },
-      locationId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'Locations',
-          key: 'id'
-        },
-        onDelete: 'CASCADE'
-      },
-      rating: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-      },
-      comment: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      }
-    }, options);
-  },
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up(queryInterface, Sequelize) {
+//     await queryInterface.createTable('Reviews', {
+//       id: {
+//         allowNull: false,
+//         autoIncrement: true,
+//         primaryKey: true,
+//         type: Sequelize.INTEGER
+//       },
+//       userId: {
+//         type: Sequelize.INTEGER,
+//         allowNull: false,
+//         references: {
+//           model: 'Users',
+//           key: 'id'
+//         },
+//         onDelete: 'CASCADE'
+//       },
+//       locationId: {
+//         type: Sequelize.INTEGER,
+//         allowNull: false,
+//         references: {
+//           model: 'Locations',
+//           key: 'id'
+//         },
+//         onDelete: 'CASCADE'
+//       },
+//       rating: {
+//         type: Sequelize.INTEGER,
+//         allowNull: false
+//       },
+//       comment: {
+//         type: Sequelize.STRING,
+//         allowNull: false
+//       },
+//       createdAt: {
+//         allowNull: false,
+//         type: Sequelize.DATE,
+//         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+//       },
+//       updatedAt: {
+//         allowNull: false,
+//         type: Sequelize.DATE,
+//         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+//       }
+//     }, options);
+//   },
 
-  async down(queryInterface, Sequelize) {
-    options.tableName = "Reviews";
-    await queryInterface.dropTable(options);
-  }
-};
+//   async down(queryInterface, Sequelize) {
+//     options.tableName = "Reviews";
+//     await queryInterface.dropTable(options);
+//   }
+// };
--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -1,58 +1,58 @@
-'use strict';
+// 'use strict';
 
-const { Model } = require('sequelize');
+// const { Model } = require('sequelize');
 
-module.exports = (sequelize, DataTypes) => {
-  class Review extends Model {
-    static associate(models) {
-      Review.belongsTo(models.User, { foreignKey: 'userId', onDelete: 'CASCADE' });
-      Review.belongsTo(models.Location, { foreignKey: 'locationId', onDelete: 'CASCADE' });
-    }
-  };
+// module.exports = (sequelize, DataTypes) => {
+//   class Review extends Model {
+//     static associate(models) {
+//       Review.belongsTo(models.User, { foreignKey: 'userId', onDelete: 'CASCADE' });
+//       Review.belongsTo(models.Location, { foreignKey: 'locationId', onDelete: 'CASCADE' });
+//     }
+//   };
 
-  Review.init({
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: { model: 'Users', key: 'id' }
-    },
-    locationId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: { model: 'Locations', key: 'id' }
-    },
-    rating: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      validate: {
-        min: 1,
-        max: 5
-      }
-    },
-    comment: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    createdAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
-    }
-  }, {
-    sequelize,
-    modelName: 'Review'
-  });
+//   Review.init({
+//     id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       autoIncrement: true,
+//       primaryKey: true
+//     },
+//     userId: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       references: { model: 'Users', key: 'id' }
+//     },
+//     locationId: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       references: { model: 'Locations', key: 'id' }
+//     },
+//     rating: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       validate: {
+//         min: 1,
+//         max: 5
+//       }
+//     },
+//     comment: {
+//       type: DataTypes.TEXT,
+//       allowNull: false
+//     },
+//     createdAt: {
+//       type: DataTypes.DATE,
+//       allowNull: false,
+//       defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+//     },
+//     updatedAt: {
+//       type: DataTypes.DATE,
+//       allowNull: false,
+//       defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+//     }
+//   }, {
+//     sequelize,
+//     modelName: 'Review'
+//   });
 
-  return Review;
-};
+//   return Review;
+// };
