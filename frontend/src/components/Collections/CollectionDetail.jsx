f8bf65a5b0dbdb0d8b4b153d524d4fc4353481ba
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-05-26 21:37:00 -0400

    Trying to fix collection images for main - works in seeds

--- frontend/src/components/Collections/CollectionDetail.jsx
+++ frontend/src/components/Collections/CollectionDetail.jsx
@@ -54,7 +54,7 @@ const CollectionDetail = () => {
       ) : (
         <div className="locations-grid">
           {Locations.map((location) => {
-            const imageUrl = location.LocationImages?.[0]?.imageUrl || ''; // Correctly accessing the imageUrl
+            const imageUrl = location.LocationImages?.[0]?.imageUrl || '';
             return (
               <div key={location.id} className="location-tile">
                 <div className="location-image">
@@ -82,45 +82,7 @@ const CollectionDetail = () => {
       )}
     </div>
   );
-
-  // return (
-  //   <div className="collection-detail-page">
-  //     <div className="collection-header">
-  //       <img src={localCollection.imageUrl} alt={localCollection.name} className="collection-image" />
-  //       <h1>{localCollection.name}</h1>
-  //     </div>
-  //     <h2>Locations in this Collection</h2>
-  //     {Locations.length === 0 ? (
-  //       <div>No Locations Added to Collection At This Time</div>
-  //     ) : (
-  //       <div className="locations-grid">
-  //         {Locations.map((location) => (
-  //           <div key={location.id} className="location-tile">
-  //             <div className="location-image">
-  //               <img src={location.image} alt={location.name} />
-  //             </div>
-  //             <div className="location-details">
-  //               <Link to={`/locations/${location.id}`}><h2>{location.name}</h2></Link>
-  //               <p>{location.description}</p>
-  //               <OpenModalButton
-  //                 buttonText="Remove Location"
-  //                 buttonClass="primary-button"
-  //                 modalComponent={
-  //                   <DeleteConfirmationModal
-  //                     onDelete={() => handleRemoveLocation(location.id)}
-  //                     itemName={location.name}
-  //                     itemType="location"
-  //                   />
-  //                 }
-  //               />
-  //             </div>
-  //           </div>
-  //         ))}
-  //       </div>
-  //     )}
-  //   </div>
-  // );
-};
+}
 
 
 export default CollectionDetail;
