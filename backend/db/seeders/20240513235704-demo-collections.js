cb349e3abc172e9ebdfd6a9d054a63b7798dc937
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-05-26 21:46:40 -0400

    Updated Collection Image Seed Data and DB Again

--- backend/db/seeders/20240513235704-demo-collections.js
+++ backend/db/seeders/20240513235704-demo-collections.js
@@ -17,35 +17,35 @@ module.exports = {
       {
         userId: 1,
         name: 'Mountain Adventures',
-        imageUrl: 'http://example.com/image/mountain.jpg',
+        imageUrl: 'https://www.travelandleisure.com/thmb/GmekR2hjot6B2mWovmI4J192nSk=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/TAL-grand-teton-USMNTNSIPOG0823-2538d183b9094e3fb59dd5e54bbe791c.jpg',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         userId: 1,
         name: 'Beach Holidays',
-        imageUrl: 'http://example.com/image/beach.jpg',
+        imageUrl: 'https://cdn.coastalscience.noaa.gov/csmedia/2022/02/View-of-St.-Croix-Island-looking-north-768x576.jpg',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         userId: 2,
         name: 'Historic Sites',
-        imageUrl: 'http://example.com/image/historic.jpg',
+        imageUrl: 'https://wisconsinexaminer.com/wp-content/uploads/2021/09/NHAL-Logging-061-1536x1024.jpg',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         userId: 3,
         name: 'Nature Walks',
-        imageUrl: 'http://example.com/image/nature.jpg',
+        imageUrl: 'https://dlnr.hawaii.gov/wp-content/uploads/2021/10/2GWLuWT4-1024x683.jpeg',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         userId: 4,
         name: 'City Breaks',
-        imageUrl: 'http://example.com/image/city.jpg',
+        imageUrl: 'https://www.fisheries.noaa.gov/s3/styles/media_750_x500/s3/2022-02/750x500-elwha-river-credit-john-mcmillan.jpg',
         createdAt: new Date(),
         updatedAt: new Date()
       }
