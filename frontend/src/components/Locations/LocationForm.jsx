56dfc16103a62f5fdffa15719437a921f855845a
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-05-28 18:36:47 -0400

    Updated Locations form to include labels and css

--- frontend/src/components/Locations/LocationForm.jsx
+++ frontend/src/components/Locations/LocationForm.jsx
@@ -1,13 +1,19 @@
-import { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate, useParams, useLocation } from 'react-router-dom';
-import { createLocation, updateLocation, fetchLocation, setErrorMessage, setLocation } from '../../store/locations';
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams, useLocation } from "react-router-dom";
+import {
+  createLocation,
+  updateLocation,
+  fetchLocation,
+  setErrorMessage,
+  setLocation,
+} from "../../store/locations";
 
 function isValidHttpUrl(string) {
-  const pattern = /^(https?:\/\/)?((([a-zA-Z\d]([a-zA-Z\d-]*[a-zA-Z\d])*)\.)+[a-zA-Z]{2,}|((\d{1,3}\.){3}\d{1,3}))(:\d+)?(\/[-a-zA-Z\d%_.~+]*)*(\?[;&a-zA-Z\d%_.~+=-]*)?(#[a-zA-Z\d_]*)?$/i;
+  const pattern =
+    /^(https?:\/\/)?((([a-zA-Z\d]([a-zA-Z\d-]*[a-zA-Z\d])*)\.)+[a-zA-Z]{2,}|((\d{1,3}\.){3}\d{1,3}))(:\d+)?(\/[-a-zA-Z\d%_.~+]*)*(\?[;&a-zA-Z\d%_.~+=-]*)?(#[a-zA-Z\d_]*)?$/i;
   return pattern.test(string);
 }
-
 
 const LocationForm = () => {
   const dispatch = useDispatch();
@@ -15,46 +21,47 @@ const LocationForm = () => {
   const { locationId } = useParams();
   const location = useLocation();
 
-  const currentLocation = useSelector((state) => state.locations.currentLocation);
-  const errorMessage = useSelector((state) => state.locations.errorMessage)
+  const currentLocation = useSelector(
+    (state) => state.locations.currentLocation
+  );
+  const errorMessage = useSelector((state) => state.locations.errorMessage);
 
   const [formData, setFormData] = useState({
-    name: '',
-    description: '',
-    activity_type: '',
-    street: '',
-    city: '',
-    state: '',
-    country: '',
-    zip_code: '',
-    latitude: '',
-    longitude: '',
-    imageUrl: '',
+    name: "",
+    description: "",
+    activity_type: "",
+    street: "",
+    city: "",
+    state: "",
+    country: "",
+    zip_code: "",
+    latitude: "",
+    longitude: "",
+    imageUrl: "",
   });
-
 
   useEffect(() => {
     if (locationId) {
       dispatch(fetchLocation(locationId));
     } else {
       setFormData({
-        name: '',
-        description: '',
-        activity_type: '',
-        street: '',
-        city: '',
-        state: '',
-        country: '',
-        zip_code: '',
-        latitude: '',
-        longitude: '',
-        imageUrl: '',
+        name: "",
+        description: "",
+        activity_type: "",
+        street: "",
+        city: "",
+        state: "",
+        country: "",
+        zip_code: "",
+        latitude: "",
+        longitude: "",
+        imageUrl: "",
       });
     }
   }, [locationId, dispatch]);
 
   useEffect(() => {
-    const isEdit = location.pathname.includes('edit')
+    const isEdit = location.pathname.includes("edit");
     if (currentLocation && isEdit) {
       setFormData(currentLocation);
     }
@@ -62,10 +69,10 @@ const LocationForm = () => {
 
   useEffect(() => {
     return () => {
-      dispatch(setLocation(null))
-      dispatch(setErrorMessage(null))
-    }
-  }, [dispatch])
+      dispatch(setLocation(null));
+      dispatch(setErrorMessage(null));
+    };
+  }, [dispatch]);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -76,15 +83,18 @@ const LocationForm = () => {
     let actionResult;
 
     if (locationId) {
-     actionResult = await dispatch(updateLocation({ id: locationId, ...formData }));
-     if (actionResult) {
-      navigate(`/locations/${currentLocation.id}`);
-     }
+      actionResult = await dispatch(
+        updateLocation({ id: locationId, ...formData })
+      );
+      if (actionResult) {
+        navigate(`/locations/${currentLocation.id}`);
+      }
     } else {
-
       if (!formData.imageUrl || !isValidHttpUrl(formData.imageUrl)) {
-        dispatch(setErrorMessage('Image Url is required and must be a valid Url'))
-        return
+        dispatch(
+          setErrorMessage("Image Url is required and must be a valid Url")
+        );
+        return;
       }
       actionResult = await dispatch(createLocation(formData));
       if (actionResult) {
@@ -95,39 +105,155 @@ const LocationForm = () => {
 
   return (
     <div className="form-container">
-      <h2 className="form-header">{locationId ? 'Edit Location' : 'Create Location'}</h2>
+      <h2 className="form-header">
+        {locationId ? "Edit Location" : "Create Location"}
+      </h2>
       <form onSubmit={handleSubmit} className="form">
-      <input type="text" name="name" placeholder="Name" value={formData.name} onChange={handleChange} required className="form-input" />
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+            className="form-input"
+          />
+        </label>
 
-        <input type="text" name="description" placeholder="Description" value={formData.description} onChange={handleChange} required className="form-input" />
+        <label>
+          Description:
+          <input
+            type="text"
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            required
+            className="form-input"
+          />
+        </label>
 
-        <select name="activity_type" value={formData.activity_type} onChange={handleChange} required className="form-input">
-          <option value="" disabled>Select Activity Type</option>
-          <option value="swimming">Swimming</option>
-          <option value="surfing">Surfing</option>
-          <option value="rafting">Rafting</option>
-          <option value="kayaking">Kayaking</option>
-        </select>
+        <label>
+          Activity Type:
+          <select
+            name="activity_type"
+            value={formData.activity_type}
+            onChange={handleChange}
+            required
+            className="form-input"
+          >
+            <option value="" disabled>
+              Select Activity Type
+            </option>
+            <option value="swimming">Swimming</option>
+            <option value="surfing">Surfing</option>
+            <option value="rafting">Rafting</option>
+            <option value="kayaking">Kayaking</option>
+          </select>
+        </label>
 
-        <input type="text" name="street" placeholder="Street" value={formData.street} onChange={handleChange} required className="form-input" />
+        <label>
+          Street:
+          <input
+            type="text"
+            name="street"
+            value={formData.street}
+            onChange={handleChange}
+            required
+            className="form-input"
+          />
+        </label>
 
-        <input type="text" name="city" placeholder="City" value={formData.city} onChange={handleChange} required className="form-input" />
+        <label>
+          City:
+          <input
+            type="text"
+            name="city"
+            value={formData.city}
+            onChange={handleChange}
+            required
+            className="form-input"
+          />
+        </label>
 
-        <input type="text" name="state" placeholder="State" value={formData.state} onChange={handleChange} required className="form-input" />
+        <label>
+          State:
+          <input
+            type="text"
+            name="state"
+            value={formData.state}
+            onChange={handleChange}
+            required
+            className="form-input"
+          />
+        </label>
 
-        <input type="text" name="country" placeholder="Country" value={formData.country} onChange={handleChange} required className="form-input" />
+        <label>
+          Country:
+          <input
+            type="text"
+            name="country"
+            value={formData.country}
+            onChange={handleChange}
+            required
+            className="form-input"
+          />
+        </label>
 
-        <input type="text" name="zip_code" placeholder="Zip Code" value={formData.zip_code} onChange={handleChange} required className="form-input"/>
+        <label>
+          Zip Code:
+          <input
+            type="text"
+            name="zip_code"
+            value={formData.zip_code}
+            onChange={handleChange}
+            required
+            className="form-input"
+          />
+        </label>
 
-        <input type="text" name="latitude" placeholder="Latitude" value={formData.latitude} onChange={handleChange} required className="form-input"/>
+        <label>
+          Latitude:
+          <input
+            type="text"
+            name="latitude"
+            value={formData.latitude}
+            onChange={handleChange}
+            required
+            className="form-input"
+          />
+        </label>
 
-        <input type="text" name="longitude" placeholder="Longitude" value={formData.longitude} onChange={handleChange} required className="form-input"/>
+        <label>
+          Longitude:
+          <input
+            type="text"
+            name="longitude"
+            value={formData.longitude}
+            onChange={handleChange}
+            required
+            className="form-input"
+          />
+        </label>
 
         {!locationId && (
-          <input type="text" name="imageUrl" placeholder="Image URL" value={formData.imageUrl} onChange={handleChange} required maxLength='255' className="form-input"/>
+          <label>
+            Image URL:
+            <input
+              type="text"
+              name="imageUrl"
+              value={formData.imageUrl}
+              onChange={handleChange}
+              required
+              maxLength="255"
+              className="form-input"
+            />
+          </label>
         )}
 
-        <button type="submit" className="form-button">{locationId ? 'Update' : 'Create'}</button>
+        <button type="submit" className="form-button">
+          {locationId ? "Update" : "Create"}
+        </button>
       </form>
       <div className="form-error">{errorMessage}</div>
     </div>
